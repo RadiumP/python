c02bde0cb1cfc08a4be85b6ee4d549ef8ba8ad85
RadiumP <chenyan5@asu.edu>
2021-09-21 17:24:41 -0700

    Remove header

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import xml.etree.ElementTree as ET
-
+from pathlib import Path
 root = ET.parse('test.ppx')
 for rule in root.iter('Rule'):
     for child in rule:
         if(child.tag == 'Name'):
             if(child.text == 'Block'):
                 print(rule.attrib['enabled'])
-                rule.attrib['enabled'] = 'true'
+                rule.attrib['enabled'] = 'false'
                 print(rule.attrib['enabled'])
-root.write('test.ppx')
+root.write('test.xml')
+
+p = Path('test.xml')
+p.rename(p.with_suffix('.ppx'))
